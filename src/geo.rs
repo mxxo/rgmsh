d57647c5c7469e930b2cc44f87b9a17fff87ab2d
Max Orok <morok@mevex.com>
2019-07-26 13:07:20 -0400

    Add geo module skeleton

---
+++ src/geo.rs
@@ -0,0 +1,12 @@
+fn addPoint(x: f64, y: f64, z: f64, mesh_size: Option<f64>, tag: Option<i32>) -> i32 {
+    1
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,12 @@ extern crate gmsh_sys;
 use std::env::Args;
 use std::io::prelude::*;
 use std::io;
+// use std::vec;
+
+mod geo;
+
+/// Geometrical entities have a dimension and a tag
+struct VectorPair(Vec<(i32, i32)>);
 
 pub fn initialize(args: Args, read_configs: bool) -> io::Result<()> {
     println!("opening Gmsh...");
@@ -13,7 +19,6 @@ pub fn finalize() -> io::Result<()> {
     println!("finalizing Gmsh...");
     Ok(())
 }
-
 
 #[cfg(test)]
 mod tests {
