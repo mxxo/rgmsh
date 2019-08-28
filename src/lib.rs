e4c316d8c5145e6cd02ef5e8d91858ed87733912
Max Orok <morok@mevex.com>
2019-08-28 11:33:27 -0400

    Try and write down all the error types used by Gmsh in various spots

--- src/lib.rs
+++ src/lib.rs
@@ -64,6 +64,37 @@ pub struct Gmsh {
 ///
 ///     }
 
+/// The different error types
+
+#[derive(Debug)]
+// Used in:
+// gmsh::model (model::geo, model::occ, model::mesh, model::geo::mesh)
+// gmsh::view
+pub enum ModelError {
+    Initialization, // -1: Gmsh wasn't initialized, or a required library component is missing
+    Mutation,       // 1: a function that mutates the model couldn't complete successfully (e.g. addPoint couldn't succeed because of a tag collision)
+    Lookup,         // 2: a data lookup getter function failed, e.g. tried to work on a view that doesn't exist
+    BadInput,       // 3: The function couldn't successfully use a required input parameter (rare) (e.g. A requested quadrature scheme couldn't be applied to the data)
+    MeshQuery,      // 4: A parallelizable mesh query function failed (rare)
+}
+
+// used by:
+// top level gmsh functions like gmsh::open, gmsh::merge, gmsh::finalize,
+// gmsh::logger
+// gmsh::onelab (?)
+// gmsh::graphics (?)
+// gmsh::fltk (?)
+pub enum GmshFileError {
+    Initialization, // -1: Gmsh wasn't initialized, or a required library component is missing
+    Execution, // 1: Gmsh couldn't perform the requested operation successfully (e.g. a bad file path was passed in)
+}
+
+// used by gmsh::option
+pub enum GmshOptionError {
+    Initialization,  // -1: Gmsh wasn't initialized, or a required library component is missing
+    UnknownOption,   // 1: The given option wasn't successfully processd, perhaps it doesn't exist
+}
+
 impl Gmsh {
     pub fn initialize(args: Args, read_configs: bool) -> io::Result<Gmsh> {
         println!("opening Gmsh...");
