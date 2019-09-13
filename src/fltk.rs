cfd2e5220b45470b82438b905a4bd18434c6fc5f
Max Orok <morok@mevex.com>
2019-09-13 10:25:02 -0400

    Add OpenGL draw method

--- src/fltk.rs
+++ src/fltk.rs
@@ -5,15 +5,12 @@
 //! # use gmsh::{Gmsh, GmshResult};
 //! # fn main() -> GmshResult<()> {
 //! let gmsh = Gmsh::initialize()?;
-//! let mut geom = gmsh.create_native_model("model")?;
+//! let mut geom = gmsh.create_occ_model("model")?;
 //!
 //! // -- add a bunch of complicated shapes
 //!
 //! // look at your model in the GUI
 //! let gui = gmsh.run_gui()?;
-//!
-//! // only resumes execution when you've closed the Gmsh window
-//!
 //! # Ok(())
 //! # }
 //! ```
@@ -22,13 +19,15 @@ use std::marker::PhantomData;
 use crate::{Gmsh, GmshError, GmshResult, check_main_error, c_int};
 
 /// The FLTK GUI object
+// TODO this needs more thought, because it's a multithreaded object
 pub struct Gui<'a> {
     phantom: PhantomData<&'a Gmsh>
 }
 
 impl Gmsh {
 
-    // will crash with "Fltk internal error" without an X display, like plain WSL
+    /// Create the GUI. If successful, open the GUI for an instant before
+    /// resuming execution.
     pub fn initialize_gui(&self) -> GmshResult<Gui> {
         let mut ierr: c_int = 0;
         unsafe {
@@ -37,6 +36,7 @@ impl Gmsh {
         check_main_error!(ierr, Gui {phantom: PhantomData})
     }
 
+    /// Run the GUI and block the calling thread until the GUI window is closed.
     pub fn run_gui(&self) -> GmshResult<Gui> {
         let mut ierr: c_int = 0;
         unsafe {
@@ -47,13 +47,22 @@ impl Gmsh {
 
 }
 
-// impl<'a> Gui<'a> {
-//
-//     pub fn run(&mut self) -> GmshResult<()> {
-//         let mut ierr: c_int = 0;
-//         unsafe {
-//             gmsh_sys::gmshFltkRun(&mut ierr);
-//         }
-//         check_main_error!(ierr, ())
-//     }
-// }
+impl<'a> Gui<'a> {
+
+    /// Draw all the OpenGL scenes
+    pub fn draw(&mut self) -> GmshResult<()> {
+        let mut ierr: c_int = 0;
+        unsafe {
+            gmsh_sys::gmshGraphicsDraw(&mut ierr);
+        }
+        check_main_error!(ierr, ())
+    }
+
+    // pub fn run(&mut self) -> GmshResult<()> {
+    //     let mut ierr: c_int = 0;
+    //     unsafe {
+    //         gmsh_sys::gmshFltkRun(&mut ierr);
+    //     }
+    //     check_main_error!(ierr, ())
+    // }
+}
